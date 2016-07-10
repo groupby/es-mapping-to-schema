f5d090fd1ccf699003cb43df2ff4d4cfc4e12db3
Eric Hacke <eric.hacke@groupbyinc.com>
2016-07-09 20:10:49 -0400

    Need to rewrite tests

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const _ = require('lodash');
+const _       = require('lodash');
+const Options = require('./options');
 
 const DIRECT_COPY_TYPES = [
   'integer',
@@ -8,100 +9,172 @@ const DIRECT_COPY_TYPES = [
   'date'
 ];
 
+const SANITIZATION_SCHEMA = 'sanization';
+const VALIDATION_SCHEMA = 'validation';
+
 // This is a Lucene limitation
 const MAX_STRING_SIZE = 32766;
 
-const RecurseMappingToSchema = (schema, mapping, modifiers) => {
-  if (mapping.properties || mapping.type === 'object') {
-    if (modifiers.isArray) {
-      schema.type  = 'array';
-      schema.items = RecurseMappingToSchema({}, mapping.properties, modifiers);
-    } else {
-      schema.type       = 'object';
+const RecurseMappingToSchema = (mapping, schema, schemaType, options, localOptions) => {
+  const optional = localOptions.optional || (options[schemaType].all.optional && !localOptions.strict);
+  const strict   = localOptions.strict || (options[schemaType].all.strict && !localOptions.optional);
 
-      if (mapping.properties) {
-        schema.properties = RecurseMappingToSchema({}, mapping.properties, modifiers);
-      }
+  localOptions = _.omit(localOptions, [
+    'strict',
+    'optional'
+  ]);
 
-      if (modifiers.allStrict && !modifiers.isOptional) {
-        schema.strict = true;
-      }
-    }
+  if (mapping.properties || mapping.type) {
+    if (mapping.properties || mapping.type === 'object' || mapping.type === 'nested') {
+      if (localOptions.isArray) {
+        schema.type  = 'array';
+        schema.items = RecurseMappingToSchema({}, mapping.properties, schemaType, options, {});
+      } else {
+        schema.type = 'object';
 
-    if (modifiers.isOptional) {
-      schema.optional = true
-    }
+        if (mapping.properties) {
+          schema.properties = RecurseMappingToSchema({}, mapping.properties, schemaType, options, {});
+        }
 
-    return schema;
-  } else if (mapping.type) {
-    if (_.includes(DIRECT_COPY_TYPES, mapping.type)) {
-      schema.type = mapping.type;
-
-      if (schema.type === 'string') {
-        schema.maxLength = MAX_STRING_SIZE;
-        schema.rules     = ['trim'];
+        if (strict) {
+          schema.strict = true;
+        }
       }
     } else {
-      switch (mapping.type) {
-        case 'double':
-        case 'float':
-          schema.type = 'number';
-          break;
-        case 'long':
-        case 'short':
-        case 'byte':
-          schema.type = 'integer';
-          break;
-        default:
-          console.log(`mapping type: ${mapping.type} is unsupported and will be ignored`);
-          break;
+      const type = convertEsTypeToSchemaType(mapping.type);
+
+      if (schemaType === SANITIZATION_SCHEMA) {
+        if (type && _.includes(options[schemaType].all.types, type)) {
+          schema.type = type;
+        }
+
+        if (schema.type === 'string' && options[schemaType].all.maxLength) {
+          schema.maxLength = options[schemaType].all.maxLength;
+        }
+
+        if (options[schemaType].all.rules) {
+          schema.rules = options[schemaType].all.rules;
+        }
+      }
+
+      if (schemaType === VALIDATION_SCHEMA && type) {
+        schema.type = type;
       }
     }
 
-    if (modifiers.isOptional) {
-      schema.optional = true
+    if (schema.type && optional) {
+      schema.optional = true;
     }
+
+    _.forEach(localOptions, (value, field) => {
+      schema[field] = value;
+    });
 
     return schema;
   } else {
-    const nextModifiers         = _.cloneDeep(modifiers);
-    nextModifiers.arrayPaths    = NextPaths(modifiers.arrayPaths);
-    nextModifiers.isArray       = false;
-    nextModifiers.optionalPaths = NextPaths(modifiers.optionalPaths);
-    nextModifiers.isOptional    = false;
+    const nextOptions             = _.cloneDeep(options);
+    nextOptions[schemaType].paths = nextPaths(nextOptions[schemaType].paths);
 
     return _.reduce(mapping, (result, property, name) => {
-      const localModifiers      = _.cloneDeep(nextModifiers);
-      localModifiers.isArray    = _.includes(modifiers.arrayPaths, name);
-      localModifiers.isOptional = _.includes(modifiers.optionalPaths, name);
+      const nextLocalOptions = getLocalOptions(options[schemaType].paths, name);
 
       schema[name] = {};
-      result[name] = RecurseMappingToSchema(schema[name], mapping[name], localModifiers);
+      result[name] = RecurseMappingToSchema(mapping[name], schema[name], schemaType, nextOptions, nextLocalOptions);
       return result;
     }, {});
   }
 };
 
-const NextPaths = currentPaths => _.reduce(currentPaths, (result, currentPath) => {
-  const nextPath = _.join(_.drop(_.split(currentPath, '.'), 1), '.');
+const getLocalOptions = (currentPathObjects, name) => _.reduce(currentPathObjects, (result, currentPathObject, field) => {
+  _.forEach(currentPathObject, specificPath => {
+    if (specificPath.path === name) {
+      result[field] = specificPath.value;
+    }
+  });
+
+  return result;
+}, {});
+
+const nextPaths = currentPathObjects => _.reduce(currentPathObjects, (result, currentPathObject, field) => {
+  result[field] = shortenPaths(currentPathObject);
+  return result;
+}, {});
+
+const shortenPaths = currentPaths => _.reduce(currentPaths, (result, currentPath) => {
+  const nextPath = _.join(_.drop(_.split(currentPath.path, '.'), 1), '.');
   if (nextPath.length > 0) {
-    result.push(nextPath);
+    result.push({
+      path:  nextPath,
+      value: currentPath.value
+    });
   }
   return result;
 }, []);
 
 const MappingToSchema = (mapping, options) => {
   options = options || {};
+  options = new Options(options);
 
-  const modifiers = {
-    arrayPaths:    options.arrayPaths,
-    allStrict:     options.allStrict && options.allStrict === true,
-    isArray:       false,
-    optionalPaths: options.optionalPaths,
-    isOptional:    false
+  return {
+    validation:   RecurseMappingToSchema(mapping, {}, VALIDATION_SCHEMA, options, {}),
+    sanitization: RecurseMappingToSchema(mapping, {}, SANITIZATION_SCHEMA, options, {})
   };
-
-  return RecurseMappingToSchema({}, mapping, modifiers);
 };
+
+const convertEsTypeToSchemaType = (type) => {
+  if (_.includes(DIRECT_COPY_TYPES, type)) {
+    return type;
+  } else {
+    switch (type) {
+      case 'double':
+      case 'float':
+        return 'number';
+      case 'long':
+      case 'short':
+      case 'byte':
+        return 'integer';
+      default:
+        console.log(`mapping type: ${type} is unsupported and will be ignored`);
+        return null;
+    }
+  }
+};
+
+const args = {
+  all:          {},
+  validation:   {
+    all:   {
+      strict:   false,
+      optional: false
+    },
+    paths: {
+      'someOverride': [
+        {
+          path:  '',
+          value: ''
+        }
+      ]
+    }
+  },
+  sanitization: {
+    all:   {
+      strict:    false,
+      rules:     [],
+      maxLength: 0,
+      types:     []
+    },
+    paths: {
+      'someOverride': [
+        {
+          path:  '',
+          value: ''
+        }
+      ]
+    }
+  }
+};
+
+MappingToSchema.__nextPaths = nextPaths;
+MappingToSchema.__getLocalOptions = getLocalOptions;
 
 module.exports = MappingToSchema;
---
+++ options.js
@@ -0,0 +1,123 @@
+const inspector = require('schema-inspector');
+
+const VALIDATION_SCHEMA = {
+  type:       'object',
+  properties: {
+    arrayPaths:        {
+      optional:   true,
+      type:       'array',
+      items: {
+        type: 'string'
+      }
+    },
+    validation:   {
+      optional:   true,
+      type:       'object',
+      properties: {
+        all:   {
+          optional:   true,
+          type:       'object',
+          properties: {
+            strict:   {
+              optional: true,
+              type:     'boolean'
+            },
+            optional: {
+              optional: true,
+              type:     'boolean'
+            }
+          }
+        },
+        paths: {
+          optional:   true,
+          type:       'object',
+          properties: {
+            '*': {
+              type:  'array',
+              items: {
+                type:       'object',
+                properties: {
+                  path:  {
+                    type: 'string'
+                  },
+                  value: {
+                    type: 'any'
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    },
+    sanitization: {
+      optional:   true,
+      type:       'object',
+      properties: {
+        all:   {
+          optional:   true,
+          type:       'object',
+          properties: {
+            strict:    {
+              optional: true,
+              type:     'boolean'
+            },
+            rules:     {
+              optional: true,
+              type:     'array'
+            },
+            maxLength: {
+              optional: true,
+              type:     'integer',
+              gte:      0
+            },
+            types:     {
+              optional: true,
+              type:     'array',
+              items:    {
+                type: 'string'
+              }
+            }
+          }
+        },
+        paths: {
+          optional:   true,
+          type:       'object',
+          properties: {
+            '*': {
+              type:  'array',
+              items: {
+                type:       'object',
+                properties: {
+                  path:  {
+                    type: 'string'
+                  },
+                  value: {
+                    type: 'any'
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+};
+
+const Options = function (params) {
+  const self = this;
+  params     = _.cloneDeep(params);
+
+  const result = inspector.validate(VALIDATION_SCHEMA, params);
+
+  if (!result.valid) {
+    throw new Error(result.format());
+  }
+
+  _.merge(self, params);
+
+  return self;
+};
+
+module.exports = Options;
